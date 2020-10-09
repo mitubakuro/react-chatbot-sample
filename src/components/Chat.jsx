2a0cfb1a99eea82e8a2b02a9533f8a678fcb5f91
mitubakuro <nakano1113mituba@gmail.com>
2020-10-10 02:09:52 +0900

    学習履歴

---
+++ src/components/Chat.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import ListItem from '@material-ui/core/ListItem';
+import ListItemAvatar from '@material-ui/core/ListItemAvatar';
+import Avatar from '@material-ui/core/Avatar';
+
+const Chat =()=>{
+  return(
+    <ListItem>
+    <ListItemAvatar>
+      <Avatar alt="icon" src="/static/images/avatar/1.jpg" />
+    </ListItemAvatar>
+    <div className="p-chat__bubble">
+      テスト
+    </div>
+  </ListItem>
+  )
+}
+
+export default Chat
